cef937f209d054fa3e3e6e6d6bc45b52284837f4
Rocco Meli <r.meli@bluemail.ch>
2022-06-19 23:09:05 +0100

    more efficient lru cache

--- lru_cache/cache.h
+++ lru_cache/cache.h
@@ -1,23 +1,35 @@
-#include <map>
+#include <deque>
 #include <tuple>
+#include <utility>
+#include <unordered_map>
+#include <optional>
 
 template <typename Result, typename... Args>
-auto lru_cache(Result (*f)(Args...)){
+auto lru_cache(Result (*f)(Args...), std::optional<size_t> cache_size = std::nullopt) {
     // Cache maps arguments to results
-    // TODO: Better data structure
-    std::map<std::tuple<Args...>, Result> cache;
+    using cache_t = std::deque<std::pair<Args..., Result>>;
+    cache_t cache;
+
+    // Maps keys to elements in the cache
+    // Provides O(1) access to the cache
+    std::unordered_map<Args..., typename cache_t::iterator> cache_map;
 
     // Capture function pointer and cache
     // Lambda is mutable in order to modify members (cache)
-    return [f, cache](Args... args) mutable -> Result {
-        const auto args_tuple = std::make_tuple(args...);
-        const auto cached = cache.find(args_tuple);
+    return [f, cache, cache_map, cache_size](Args... args) mutable -> Result {
+        const auto cached_map_it = cache_map.find(args...);
 
-        if(cached == cache.end()){
+        if(cached_map_it == cache_map.end()){ // Not found in cache
+            if(cache_size && cache.size() == *cache_size){ // Cache is full
+                cache_map.erase(cache.front().first); // Remove oldest element from map
+                cache.pop_front(); // Remove oldest element from cache
+            }
             const auto result = f(args...);
-            cache[args_tuple] = result;  // Cache  uncached result
+            cache.emplace_front(std::make_pair(args..., result));
+            cache_map.emplace(args..., cache.end() - 1); // End iterator points after last element
             return result;
         }
-        return cached->second; // chached is an iterator to a pair<args, result>
+
+        return cached_map_it->second->second;
     };
 }
