7bc9ea5329ad64a76e3ed546c99434dff4354325
Rocco Meli <r.meli@bluemail.ch>
2026-01-29 11:34:49 +0100

    comment

--- dlopen/lib.cpp
+++ dlopen/lib.cpp
@@ -3,6 +3,10 @@
 // Without extern "C", the function name is mangled:
 //     $ nm -gD build/liblib.so | grep add
 //     00000000000010f9 T _Z3addii
+//
+// With extern "C", the function name is not mangled:
+//     $ nm -gD build/liblib.so | grep add
+//     00000000000010f9 T add
 
 extern "C" {
 
